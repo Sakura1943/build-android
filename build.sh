c578cc09d562edd1d501418e323f9b3832bfde76
Sakura1943 <62229444+Sakura1943@users.noreply.github.com>
2022-08-27 20:50:01 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+mkdir -p ~/bin
+mkdir -p ~/android/lineage
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+cd ~/android/lineage
+~/bin/repo init -u https://github.com/LineageOS/android.git -b lineage-19.1
+~/bin/repo sync 
+tar -czvf cd ~/android/lineage -C /root/file.tar.gz
